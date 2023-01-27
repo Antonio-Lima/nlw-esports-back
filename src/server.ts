2f8e39a8aad2e5d9ea774d8af7692b0f977a469d
AntonioLima <gutto1998@gmail.com>
2023-01-27 16:04:48 -0300

    - Post Route created:   "/ads/:gameId" - Route to post a new ad.
    - Created method to convert hour into minutes.

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,33 @@
-import { PrismaClient } from '@prisma/client';
-import express from 'express';
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+import { convertHourStringToMinutes } from "./utils/convertHours";
 
 const app = express();
 
-const prisma = new PrismaClient();
+app.use(express.json());
 
 app.listen(3333);
 
-// Métodos GET
+const prisma = new PrismaClient();
 
-app.get('/games', async (req, res) => {
+// GET Methods
+
+app.get("/games", async (req, res) => {
   const games = await prisma.game.findMany({
     include: {
       _count: {
         select: {
           ads: true,
-        }
-      }
-    }
-  })
+        },
+      },
+    },
+  });
 
-  return res.json(games)
+  return res.json(games);
 });
 
-app.get('/games/:id/ads', async (req, res) => {
+app.get("/games/:id/ads", async (req, res) => {
   const gameId = req.params.id;
 
   const ads = await prisma.ads.findMany({
@@ -37,17 +41,17 @@ app.get('/games/:id/ads', async (req, res) => {
       hourEnd: true,
     },
     where: {
-      gameId
+      gameId,
     },
     orderBy: {
-      createdAt: 'desc'
-    }
+      createdAt: "desc",
+    },
   });
 
   return res.json(ads);
 });
 
-app.get('/ads/:id/discord', async (req, res) => {
+app.get("/ads/:id/discord", async (req, res) => {
   const adId = req.params.id;
 
   const discord = await prisma.ads.findUniqueOrThrow({
@@ -56,8 +60,30 @@ app.get('/ads/:id/discord', async (req, res) => {
     },
     where: {
       id: adId,
-    }
+    },
   });
 
   return res.json(discord);
-})
+});
+
+// POST Methods
+
+app.post("/ads/:gameId", async (req, res) => {
+  const gameId = req.params.gameId;
+  const body: any = req.body;
+
+  const ad = await prisma.ads.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(","),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return res.status(201).json(ad);
+});
---
+++ src/utils/convertHours.ts
@@ -0,0 +1,7 @@
+export function convertHourStringToMinutes(hourString: string) {
+  const [hours, minutes] = hourString.split(":");
+
+  const minutesAmount = Number(hours) * 60 + Number(minutes);
+
+  return minutesAmount;
+}
